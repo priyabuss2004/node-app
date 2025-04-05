267b44f42dcdab1680badeeabb0495783fe772a9
priyanka <priyabuss2004@gmail.com>
2025-04-06 02:54:40 +0530

    new route path added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const os = require('os');
 
 const app = express();
 
@@ -10,6 +11,23 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get('/path', (req, res) => {
+  const hostname = os.hostname();
+  const networkInterfaces = os.networkInterfaces();
+  let ipAddress = 'Not found';
+
+  for (const interfaceName in networkInterfaces) {
+    const interfaces = networkInterfaces[interfaceName];
+    for (const iface of interfaces) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        ipAddress = iface.address;
+        break;
+      }
+    }
+  }
+  res.send(`Hostname: ${hostname}, IP Address: ${ipAddress}`);
 });
 
 app.listen(3000, () => {
